51ad7a871371ffea5587a53c949489bd843a9123
bonochof <ryos427@yahoo.co.jp>
2018-10-03 15:08:02 +0900

    fix create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
   def new
   end
-  
+
   def create
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
     # raise params.inspect
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
